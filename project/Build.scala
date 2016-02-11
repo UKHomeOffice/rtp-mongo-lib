d1df002d0ffb0cec47a610c87ec6b98460e6665c
David Ainslie <dainslie@gmail.com>
2016-02-11 14:31:04 +0000

    Upgrade

--- project/Build.scala
+++ project/Build.scala
@@ -64,8 +64,8 @@ object Build extends Build {
       libraryDependencies ++= Seq(
         "uk.gov.homeoffice" %% "rtp-io-lib" % "1.2.0-SNAPSHOT" withSources(),
         "uk.gov.homeoffice" %% "rtp-io-lib" % "1.2.0-SNAPSHOT" % Test classifier "tests" withSources(),
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0" withSources(),
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0" % Test classifier "tests" withSources()
+        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.0-SNAPSHOT" withSources(),
+        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.0-SNAPSHOT" % Test classifier "tests" withSources()
       )
     )
   }
