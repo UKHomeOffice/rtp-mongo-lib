c1d618df5a7a3df6f4146c6d56ff298a80e2b4f2
David Ainslie <dainslie@gmail.com>
2016-02-23 09:01:29 +0000

    Salat context set as implicit on Repository constructor. It defaults to Salat's default.

--- src/main/scala/uk/gov/homeoffice/mongo/salat/Repository.scala
+++ src/main/scala/uk/gov/homeoffice/mongo/salat/Repository.scala
@@ -1,11 +1,11 @@
 package uk.gov.homeoffice.mongo.salat
 
 import org.bson.types.ObjectId
+import com.novus.salat.Context
 import com.novus.salat.dao.{SalatDAO, ModelCompanion}
-import com.novus.salat.global.ctx
 import uk.gov.homeoffice.mongo.casbah.Mongo
 
-abstract class Repository[M <: AnyRef with Product](implicit m: Manifest[M]) extends ModelCompanion[M, ObjectId] with Mongo {
+abstract class Repository[M <: AnyRef with Product](implicit c: Context = com.novus.salat.global.ctx, m: Manifest[M]) extends ModelCompanion[M, ObjectId] with Mongo {
   val collectionName: String
 
   lazy val dao = new SalatDAO[M, ObjectId](db(collectionName)) {}
