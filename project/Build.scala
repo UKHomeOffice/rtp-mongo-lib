654bb8fe4be23a46e74e755a2bd1e1e1d92d9a1a
David Ainslie <dainslie@gmail.com>
2016-02-24 12:30:35 +0000

    Version 1.3.0-SNAPSHOT with a proper Build

--- project/Build.scala
+++ project/Build.scala
@@ -33,39 +33,15 @@ object Build extends Build {
         "Kamon Repository" at "http://repo.kamon.io"),
       libraryDependencies ++= Seq(
         "com.novus" %% "salat" % "1.9.9",
-        "de.flapdoodle.embed" % "de.flapdoodle.embed.mongo" % "1.50.2" withSources()
+        "de.flapdoodle.embed" % "de.flapdoodle.embed.mongo" % "1.50.2" withSources(),
+        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.2.0-SNAPSHOT" withSources(),
+        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.0-SNAPSHOT" withSources()
       ),
       libraryDependencies ++= Seq(
+        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.2.0-SNAPSHOT" % Test classifier "tests" withSources(),
+        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.0-SNAPSHOT" % Test classifier "tests" withSources()
       )
     )
     //.settings(javaOptions += "-Dconfig.resource=application.test.conf")
     .settings(run := (run in Runtime).evaluated) // Required to stop Gatling plugin overriding the default "run".
-
-  val ioPath = "../rtp-io-lib"
-  val testPath = "../rtp-test-lib"
-
-  val root = if (file(ioPath).exists && sys.props.get("jenkins").isEmpty) {
-    println("=============")
-    println("Build Locally")
-    println("=============")
-
-    val io = ProjectRef(file(ioPath), "rtp-io-lib")
-    val test = ProjectRef(file(testPath), "rtp-test-lib")
-
-    mongo.dependsOn(io % "test->test;compile->compile")
-         .dependsOn(test % "test->test;compile->compile")
-  } else {
-    println("================")
-    println("Build on Jenkins")
-    println("================")
-
-    mongo.settings(
-      libraryDependencies ++= Seq(
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.2.0-SNAPSHOT" withSources(),
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.2.0-SNAPSHOT" % Test classifier "tests" withSources(),
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.0-SNAPSHOT" withSources(),
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.0-SNAPSHOT" % Test classifier "tests" withSources()
-      )
-    )
-  }
 }
