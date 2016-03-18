bb15745d88d6d6ed966e12be996d52fabfe07dc2
David Ainslie <dainslie@gmail.com>
2016-03-18 15:14:33 +0000

    Version 1.7.1

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.7.0",
+      version := "1.7.1",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
@@ -39,8 +39,8 @@ object Build extends Build {
     )
     .settings(libraryDependencies ++= {
       val `gatling-verson` = "2.1.7"
-      val `rtp-io-lib-version` = "1.6.0"
-      val `rtp-test-lib-version` = "1.2.0"
+      val `rtp-io-lib-version` = "1.7.1"
+      val `rtp-test-lib-version` = "1.2.1"
 
       Seq(
         "com.novus" %% "salat" % "1.9.9",
