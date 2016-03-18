733a31ea3802aebd46f5bd80c3f0b494e1eadde4
David Ainslie <dainslie@gmail.com>
2016-03-18 16:57:29 +0000

    Latest version of io-lib

--- project/Build.scala
+++ project/Build.scala
@@ -39,7 +39,7 @@ object Build extends Build {
     )
     .settings(libraryDependencies ++= {
       val `gatling-verson` = "2.1.7"
-      val `rtp-io-lib-version` = "1.7.1"
+      val `rtp-io-lib-version` = "1.7.2"
       val `rtp-test-lib-version` = "1.2.1"
 
       Seq(
