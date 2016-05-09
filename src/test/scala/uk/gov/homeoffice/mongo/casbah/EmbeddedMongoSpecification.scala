1f6782b900596644750551c2416a95a8744a8a39
David Ainslie <dainslie@gmail.com>
2016-05-09 15:05:51 +0100

    Removed obsolete MongoClient

--- src/test/scala/uk/gov/homeoffice/mongo/casbah/EmbeddedMongoSpecification.scala
+++ src/test/scala/uk/gov/homeoffice/mongo/casbah/EmbeddedMongoSpecification.scala
@@ -1,8 +1,7 @@
 package uk.gov.homeoffice.mongo.casbah
 
-import scala.util.Try
 import com.mongodb.ServerAddress
-import com.mongodb.casbah.MongoDB
+import com.mongodb.casbah.{MongoClient, MongoDB}
 import org.specs2.execute.{AsResult, Result}
 import org.specs2.mutable.SpecificationLike
 import org.specs2.specification.AroundEach
@@ -12,10 +11,16 @@ import org.specs2.specification.AroundEach
  * Every running example will be given its own unique instance of Mongo.
  * If you can, it is recommended to use EmbeddedMongo instead, as it is mixed in at the example level and can aid code readability.
  */
-trait EmbeddedMongoSpecification extends AroundEach with EmbeddedMongoExecutable with EmbeddedMongoClient {
-  this: SpecificationLike =>
+trait EmbeddedMongoSpecification extends AroundEach with Mongo with EmbeddedMongoExecutable {
+  spec: SpecificationLike =>
 
   isolated
+
+  lazy val database = "embedded-database"
+
+  lazy val mongoClient = MongoClient(new ServerAddress(network.getServerAddress, network.getPort))
+
+  lazy val db = mongoClient(database)
 
   override def around[T: AsResult](t: => T): Result = try {
     debug("Starting Mongo...")
@@ -25,35 +30,8 @@ trait EmbeddedMongoSpecification extends AroundEach with EmbeddedMongoExecutable
     debug("Stopping Mongo")
     mongodExecutable.stop()
   }
-}
-
-trait EmbeddedMongoClient extends Mongo with AroundEach {
-  client: EmbeddedMongoSpecification =>
-
-  lazy val database = "embedded-database"
-
-  lazy val mongoClient = com.mongodb.casbah.MongoClient(new ServerAddress(network.getServerAddress, network.getPort))
-
-  lazy val db = mongoClient(database)
-
-  override protected def around[R: AsResult](r: => R): Result = try {
-    dropDatabase()
-    AsResult(r)
-  } finally {
-    dropDatabase()
-  }
-
-  private def dropDatabase() = Try {
-    mongoClient getDatabaseNames()
-  } map {
-    _.map {
-      mongoClient.getDB
-    } foreach {
-      _.dropDatabase()
-    }
-  }
 
   trait TestMongo extends Mongo {
-    lazy val db: MongoDB = client.db
+    lazy val db: MongoDB = spec.db
   }
 }
