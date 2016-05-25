221c1dee29f266db43782464735f83c1b53c5c0e
David Ainslie <dainslie@gmail.com>
2016-05-25 19:58:18 +0100

    Cleanup

--- src/main/scala/uk/gov/homeoffice/mongo/salat/Repository.scala
+++ src/main/scala/uk/gov/homeoffice/mongo/salat/Repository.scala
@@ -30,7 +30,6 @@ import uk.gov.homeoffice.mongo.casbah.Mongo
   * </pre>
   */
 abstract class Repository[M <: AnyRef with Product](implicit m: Manifest[M]) extends ModelCompanion[M, ObjectId] with Mongo {
-
   com.mongodb.casbah.commons.conversions.scala.RegisterJodaTimeConversionHelpers()
 
   val collectionName: String
