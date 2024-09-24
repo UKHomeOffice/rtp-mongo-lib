da3bc97775df4ee31d7331c5af91966e58fafbc9
Phillip Taylor <phillip.taylor@transformuk.com>
2024-09-24 10:15:21 +0100

    DPSPS-50: Configure unit tests to run with a database in CI environment

--- src/test/scala/uk/gov/homeoffice/mongo/TestMongo.scala
+++ src/test/scala/uk/gov/homeoffice/mongo/TestMongo.scala
@@ -6,8 +6,9 @@ object TestMongo {
     val randomDB = java.util.UUID.randomUUID().toString()
     println(s"Creating Test database connection to database $randomDB")
 
+    val dbHost = scala.util.Try(sys.env("DB_TEST_HOST")).toOption.getOrElse("localhost")
     MongoConnector.connect(
-      s"mongodb://localhost/$randomDB",
+      s"mongodb://$dbHost/$randomDB",
       "Test",
       false,
       randomDB
